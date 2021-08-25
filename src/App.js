a272a61f90edf2fffc51fbb11201718006edf2a0
adhikarikhagen <aadhikarikhagen30@gmail.com>
2021-08-26 01:19:57 +0545

    smile-dental

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <img src="http://lorempixel.com/400/200" />
-    </div>
-  );
+
+    return (
+        <div className="App" >
+            <header>
+                <a href="https://www.facebook.com/prabhat.shrestha.3557">Go To my facebook page</a>
+            </header>
+            <img src="1.jpg" />
+            <img src="2.jpg" />
+            <img src="3.jpg" />
+            <img src="4.jpg" />
+            <img src="5.jpg" />
+            <img src="6.jpg" />
+            <img src="7.jpg" />
+            <img src="8.jpg" />
+            <img src="9.jpg" />
+            <img src="10.jpg" />
+            <img src="11.jpg" />
+            <p>We offer you an exceptional dental treatment that you have been looking for from highly experienced NMC registered Dental doctors.Our aim is to help each individual in attaining and maintaing long term oral health and healthy beautiful smile you deserve.</p>
+            <footer>
+                <a href="https://www.facebook.com/Prabhat19477">Visit our facebook page for more information</a>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
